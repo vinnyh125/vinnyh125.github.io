608c014ca3b1f9edafde8cea123aeb89dae33ef3
NotVinnny <vh222@cornell.edu>
2023-07-04 22:36:17 -0400

    Included cool hover effect
    +used javascript to do cool glitch effect
    +made chinese name change opacity to visible

---
+++ scripts.js
@@ -0,0 +1,30 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.getElementById("english-name").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+      document.getElementById("chinese-name").style.opacity = "0.5";
+      document.getElementById("chinese-name").style.transition = "opacity 3s";
+    }
+    
+    iteration += 1 / 3;
+  }, 50);
+}

aae8a9f7a6666e919e40e38b90c03d7c1267213b
NotVinnny <vh222@cornell.edu>
2023-07-07 04:45:54 -0400

    Animation stuff without favicons

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ $(document).ready(function () {
 
     let interval = null;
 
+    let firstTime = true;
+
+    let navbarHeight = document.getElementById("navbar").offsetHeight;
+
     document.getElementById("english-name").onmouseover = event => { // I think I used some redundant code which I will fix in the future :^)
         if (mouseOver % 4 === 0) {
             let iteration = 0;
@@ -35,7 +39,101 @@ $(document).ready(function () {
                     document.getElementById("navbar").style.animationName = "slideUp";
                     document.getElementById("navbar").style.animationDuration = "1s";
                     mouseOver++;
-                    console.log(mouseOver);
+                    if (firstTime) {
+                        document.getElementById("cover").style.display = "block";
+                        document.getElementById("cover").style.top = navbarHeight + "px";
+                        setTimeout(() => {
+                            let iteration = 0;
+
+                            clearInterval(interval);
+                
+                            interval = setInterval(() => {
+                                event.target.innerText = student.split("")
+                                    .map((letter, index) => {
+                                        if (index < iteration) {
+                                            return student.split("")[index];
+                                        }
+                
+                                        return letters[Math.floor(Math.random() * 26)];
+                                    })
+                                    .join("");
+                
+                                if (iteration >= student.split("").length) {
+                                    clearInterval(interval);
+                                }
+                                iteration += 1 / 2;
+                            }, 50);
+                        }, 3000);
+                        setTimeout(() => {
+                            let iteration = 0;
+
+                            clearInterval(interval);
+                
+                            interval = setInterval(() => {
+                                event.target.innerText = music.split("")
+                                    .map((letter, index) => {
+                                        if (index < iteration) {
+                                            return music.split("")[index];
+                                        }
+                
+                                        return letters[Math.floor(Math.random() * 26)];
+                                    })
+                                    .join("");
+                
+                                if (iteration >= music.split("").length) {
+                                    clearInterval(interval);
+                                }
+                                iteration += 1 / 3;
+                            }, 50);
+                        }, 6000);
+                        setTimeout(() => {
+                            let iteration = 0;
+
+                            clearInterval(interval);
+                
+                            interval = setInterval(() => {
+                                event.target.innerText = badminton.split("")
+                                    .map((letter, index) => {
+                                        if (index < iteration) {
+                                            return badminton.split("")[index];
+                                        }
+                
+                                        return letters[Math.floor(Math.random() * 26)];
+                                    })
+                                    .join("");
+                
+                                if (iteration >= badminton.split("").length) {
+                                    clearInterval(interval);
+                                }
+                                iteration += 1 / 2;
+                            }, 50);
+                        }, 9000);
+                        setTimeout(() => {
+                            let iteration = 0;
+                            
+                            clearInterval(interval);
+
+                            interval = setInterval(() => {
+                                event.target.innerText = name.split("")
+                                    .map((letter, index) => {
+                                        if (index < iteration) {
+                                            return name.split("")[index];
+                                        }
+
+                                        return letters[Math.floor(Math.random() * 26)];
+                                    })
+                                    .join("");
+                                if (iteration >= name.split("").length) {
+                                    clearInterval(interval);
+                                }
+                                iteration += 1 / 3;
+                            }, 50);
+                        }, 12000);
+                        setTimeout(() => {
+                            document.getElementById("cover").style.display = "none";
+                        }, 14400);
+                        firstTime = false;
+                    }
                 }
                 iteration += 1 / 3;
             }, 50);
